9bec796b57ab57f3d67c1bc4932299b44e0a04a2
Namrone <151798569+Namrone@users.noreply.github.com>
2025-10-09 18:22:59 -0700

    Changed @board variable to hash

--- connect_CLI.rb
+++ connect_CLI.rb
@@ -2,8 +2,12 @@ class ConnectFour
   attr_accessor :board, :ranks, :files
 
   def initialize
-    @board = Array.new(6,["\u25EF","\u25EF","\u25EF","\u25EF","\u25EF","\u25EF","\u25EF"])
+    @board = (1..6).to_h {|i| [i,Array.new(7,"\u25EF")]}
     @ranks = [1,2,3,4,5,6]
     @files = ['A','B','C','D','E','F','G']
+  end
+
+  def files_convert(file)
+    files.index(file)
   end
 end
--- spec/connect_CLI_spec.rb
+++ spec/connect_CLI_spec.rb
@@ -1,11 +1,11 @@
 require_relative '../connect_CLI'
 describe ConnectFour do
   describe '#initialize' do
-    context 'print out the correct size board' do
+    context 'initialize the correct size board' do
       subject(:game){described_class.new}      
       it 'is 7 slots in a row' do
         circle = "\u25EF"
-        expect(game.board[0]).to eq([circle,circle,circle,circle,circle,circle,circle])
+        expect(game.board[1]).to eq([circle,circle,circle,circle,circle,circle,circle])
       end
 
       it 'is 6 rows total' do
@@ -13,14 +13,38 @@ describe ConnectFour do
       end
     end
 
-    context 'print out the identifier for the ranks/files of the board' do
+    context 'initialize the identifier for the ranks/files of the board' do
       subject(:game){described_class.new}   
       it 'the files for the board should display a-g' do
         expect(game.files).to eq(['A','B','C','D','E','F','G'])
       end
 
-      it 'the ranks for the board shoudl display 1-6' do
+      it 'the ranks for the board should display 1-6' do
         expect(game.ranks).to eq([1,2,3,4,5,6])
+      end
+    end
+  end
+
+  describe '#convert files' do
+    context 'convert the files location in alphabetic form and converts to numeric' do
+      subject(:game){described_class.new}
+      it 'converts A to 0' do
+        expect(game.files_convert('A')).to eq(0)
+      end
+
+      it 'converts G to 6' do
+        expect(game.files_convert('G')).to eq(6)
+      end
+    end
+
+    context 'returns nil for something besies A-G entry' do
+      subject(:game){described_class.new}
+      it 'returns P to nil' do
+        expect(game.files_convert('P')).to be_falsey
+      end
+
+      it 'returns 3 to nil' do
+        expect(game.files_convert(3)).to be_falsey
       end
     end
   end
